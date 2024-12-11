b1e78f02894d95f777578ec204df1079e68e76da
Lewis Matthews <madlou@gmail.com>
2024-12-11 23:59:44 +0000

    added notes for cron

--- renew-certs.sh
+++ renew-certs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/eclipse-workspace/www
 docker-compose run --rm certbot renew
+
+# force cert renewal
 # docker-compose run --rm certbot renew --force-renewal
+
+# crontabs
+# 0 0 * * * date >> /home/ubuntu/dev/slimstore-infrastructure/renew-certs.log
+# 1 0 * * * /home/ubuntu/dev/slimstore-infrastructure/renew-certs.sh >> /home/ubuntu/dev/slimstore-infrastructure/renew-certs.log
