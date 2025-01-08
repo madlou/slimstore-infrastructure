be7c876c02a66f84c133a329558d4e4e4e88ab3c
Lewis Matthews <madlou@gmail.com>
2025-01-08 10:19:35 +0000

    updated deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,43 @@ else
     COMPOSE_PROJECT_NAME=slimstore-$1 docker compose -f $1/docker-compose.yaml down
     COMPOSE_PROJECT_NAME=slimstore-$1 docker compose -f $1/docker-compose.yaml up -d --no-build
 fi
+if [ $1 = "dev" ]
+then
+    cd ..
+    if [ ! -e "slimstore-backend" ]
+    then
+        git clone git@github.com:madlou/slimstore-backend.git slimstore-backend
+    else
+        cd slimstore-backend
+        git pull
+        cd ..
+    fi
+    if [ ! -e "slimstore-frontend" ]
+    then
+        git clone git@github.com:madlou/slimstore-frontend.git slimstore-frontend
+        cd slimstore-frontend
+        npm install
+        cd ..
+    else
+        cd slimstore-frontend
+        git pull
+        cd ..
+    fi
+    if [ ! -e "slimstore-customerdisplay" ]
+    then
+        git clone git@github.com:madlou/slimstore-customerdisplay.git slimstore-customerdisplay
+        cd slimstore-customerdisplay
+        npm install
+        cd ..
+    else
+        cd slimstore-customerdisplay
+        git pull
+        cd ..
+    fi
+    cd slimstore-infrastructure
+    echo "*********"
+    echo "Open backend in Eclipse and run in IDE."
+    echo "Open frontend in VS Code and run in IDE."
+    echo "Open customerdisplay in VS Code and run in IDE."
+    echo "*********"
+fi
