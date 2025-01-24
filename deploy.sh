d1ff3648e90cfa0e9a7cb44a8928c72a49cf0b44
Lewis Matthews <madlou@gmail.com>
2025-01-24 16:37:00 +0000

    added deploy service parameter

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ then
 else
     date +"%Y-%m-%d %H:%M:%S" >> deploy.log
     echo "Deployed $1" >> deploy.log
-    COMPOSE_PROJECT_NAME=slimstore-$1 docker compose -f $1/docker-compose.yaml build
-    COMPOSE_PROJECT_NAME=slimstore-$1 docker compose -f $1/docker-compose.yaml down
-    COMPOSE_PROJECT_NAME=slimstore-$1 docker compose -f $1/docker-compose.yaml up -d --no-build
+    COMPOSE_PROJECT_NAME=slimstore-$1 docker compose -f $1/docker-compose.yaml build $2 $3 $4 $5 $6
+    COMPOSE_PROJECT_NAME=slimstore-$1 docker compose -f $1/docker-compose.yaml down $2 $3 $4 $5 $6
+    COMPOSE_PROJECT_NAME=slimstore-$1 docker compose -f $1/docker-compose.yaml up -d --no-build $2 $3 $4 $5 $6
 fi
 if [ $1 = "dev" ]
 then
